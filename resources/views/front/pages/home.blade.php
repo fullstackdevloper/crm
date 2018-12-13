5d642565f2232ce0ca7323b1a2c98273d957d9b0
sanjeev <upworksanjeev@gmail.com>
2018-12-13 18:19:32 +0530

    bug fixing

--- resources/views/front/pages/home.blade.php
+++ resources/views/front/pages/home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
  <section class="content_holder">
-      <h1 style="text-align:center; margin-top:100px; text-decoration:none;">CRM Glocify Technology</h1>
+      <h1 style="text-align:center; margin-top:100px; text-decoration:none;" class="heading-glocify">CRM Glocify Technology</h1>
       <h3 style="text-align:center; margin-top:20; text-decoration:none;"><a href="/admin">Admin</a></h3>
       <h3 style="text-align:center; margin-top:20; text-decoration:none;"><a href="/manager">Manager</a></h3>
       <h3 style="text-align:center; margin-top:20; text-decoration:none;"><a href="bidder">Bidder</a></h3>
