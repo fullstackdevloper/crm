e6628da2809d97732b2c3febf34fe0633a744887
sanjeev <upworksanjeev@gmail.com>
2018-12-14 22:02:03 +0530

    add filter for crm

--- resources/views/front/front_head.blade.php
+++ resources/views/front/front_head.blade.php
@@ -7,7 +7,7 @@
       <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
       <title> Management</title>
       <!-- Bootstrap -->
-      <link href="{{ URL::asset('front/css/bootstrap.min.css') }}" rel="stylesheet">
+      <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
       <link href="{{ URL::asset('front/css/custom.css') }}" rel="stylesheet">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
@@ -16,12 +16,18 @@
      
      
 	  @yield("content")
-	  <footer>
-         <p style="text-align:center;" class="copyr">©Management 2018 - All rights reserved</p>
-      </footer>
-      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<footer id="footer">	
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<span class="management"> ©Management 2018 - All rights reserved</span>
+				</div>
+			</div>
+		</div>
+    </footer> 
+      <script src="{{ URL::asset('front/js/bootstrap.min.js') }}"></script>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-      <!-- Include all compiled plugins (below), or include individual files as needed -->
-      <script src="assets/js/bootstrap.min.js"></script>
+	  
+
    </body>
 </html>
--- resources/views/front/pages/home.blade.php
+++ resources/views/front/pages/home.blade.php
@@ -1,13 +1,60 @@
 @extends('front.front_head')
 @section('content')
 
- <section class="content_holder">
-      <h1 style="text-align:center; margin-top:100px; text-decoration:none;" class="heading-glocify">CRM Glocify Technology</h1>
-      <h3 style="text-align:center; margin-top:20; text-decoration:none;"><a href="/admin">Admin</a></h3>
-      <h3 style="text-align:center; margin-top:20; text-decoration:none;"><a href="/manager">Manager</a></h3>
-      <h3 style="text-align:center; margin-top:20; text-decoration:none;"><a href="bidder">Bidder</a></h3>
 	  
-      </section>
-      
+	 <!-- page-header -->
+    <div class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div class="page-caption">
+                        <h1 class="page-title">CRM Glocify Technology</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.page-header-->
+    <!-- news -->
+    <div class="card-section">
+        <div class="container">
+            <div class="bg-white">
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <!-- section-title -->
+                        <div class="section-title mb-0">
+                           <ul class="ullink">
+								<li><a href="/admin">Admin</a></li>
+								<li><a href="/manager">Manager</a></li>
+								<li><a href="bidder">Bidder</a></li>
+							</ul>
+                        </div>
+                        <!-- /.section-title -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<style>
+ul.ullink { margin: 0; padding: 0; text-align: center;}
+ul.ullink li {list-style: none; display: inline-block; margin: 0 10px 0;}
+ul.ullink li a { background: #F15D22; color: #fff; padding: 12px 50px; border-radius: 30px; text-decoration:none; text-transform: uppercase;}
+ul.ullink li a:hover{background: #f47521;}
+footer#footer {position: fixed;bottom: 0;background: #333;width: 100%;padding: 20px 0;}
+.management{color: #fff;}
+
+.page-header { background: url(https://easetemplate.com/free-website-templates/hike/images/pageheader.jpg)no-repeat; position: relative; background-size: cover; }
+.page-caption { padding-top: 100px; padding-bottom: 174px; }
+.page-title { font-size: 46px; line-height: 1; color: #fff; font-weight: 600; text-align: center;text-transform: uppercase;
+    text-shadow: 0px 5px 5px rgba(0,0,0,0.8); }
+.card-section { position: relative; bottom: 60px; }
+.bg-white{background: #f5f5f5 !important; height: 100%; box-shadow: 0 0 30px rgba(0,0,0,0.2); border: 1px solid #fff;     padding: 80px 0; width: 100%; max-width: 700px; margin: 0 auto;}
+
+
+
+</style>
+			
 
  @endsection       
